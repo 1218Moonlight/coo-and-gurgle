ee408aac2ce1744bb67b0a4522b85e7f459ef057
kmoon <1218moonlight@gmail.com>
2019-05-08 12:09:45 +0900

    ADD Extension Checker

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 """ python3 """
 
 from src import vm
+import sys, os
 
 
-def main():
-    with open('test.cag', 'r', encoding='UTF8') as f:
+def cagRun():
+    cagPath = sys.argv[1]
+    extension = os.path.splitext(cagPath)[1].replace(".", "")
+    if not extension == "cag":
+        raise RuntimeError("Error Extension 'not .cag'")
+
+    with open(cagPath, 'r', encoding='UTF8') as f:
         vm.machine(f).run()
 
 
+def repl():
+    print("...")
+
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        cagRun()
+    else:
+        repl()
